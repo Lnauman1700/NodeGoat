eb1b420aa52cef6d0adb650a893a08d231ff775d
Lucas Nauman <Lnauman1700@gmail.com>
2019-03-11 15:54:26 -0400

    added a maxAge to the cookie session so that users can only be logged in for 2 hours max

--- server.js
+++ server.js
@@ -98,6 +98,7 @@ MongoClient.connect(config.db, function(err, db) {
         // TODO: Add "maxAge"
         cookie: {
             httpOnly: true,
+            maxAge: 2 * 60 * 60 * 1000,
             // Remember to start an HTTPS server to get this working
             //secure: true
         }
